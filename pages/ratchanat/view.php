e5dc50f912c1427f1910537a7549e73466ae97fe
azsos741 <53871216+azsos741@users.noreply.github.com>
2020-11-24 17:58:24 +0700

    Art
    Art

--- pages/ratchanat/view.php
+++ pages/ratchanat/view.php
@@ -65,7 +65,7 @@
                         <p>Starting with Discover, children are introduced to the fundamentals of STEM while working together to solve fun challenge and buildubg models using LEGO DUPLO bricks.</p>
                         <p>As participants progress into Explore, children will take their background knowledge of STEM and put it into parctice as they work in teams to design and build robots using the LEGO Education WeDo 2.0 system</p>
                         <p>Once they are ready to move into a competitive setting, they join Challenge and apply their STEM skills combined with critical thinking to work with a team, build a robot, and compete in an exciting, mission based robotics game.</p>
-						<a href=""  class="Button" style="padding: 4px;  margin: 30px; width: 30%; text-align: center; background: linear-gradient(90deg, #0056f7 0%, #ff3ee7 100%);    border: 1px solid #ffffff;" >Enroll</a>
+						<a href=""  class="Button" style="padding: 4px;  margin: 30px; width: 30%; text-align: center; background: linear-gradient(90deg, #0056f7 0%, #ff3ee7 100%);    border: 1px solid #ffffff; margin-left:0px;" >Enroll</a>
                     </div>	
                 </div>
             </div>

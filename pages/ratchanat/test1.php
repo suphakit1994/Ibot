0ec6b5c1d8d263d360ee80463a8010b3f8f5486c
azsos741 <53871216+azsos741@users.noreply.github.com>
2020-11-24 17:20:26 +0700

    Art 24/11/2020
    24/11/2020

--- pages/ratchanat/test1.php
+++ pages/ratchanat/test1.php
@@ -68,11 +68,11 @@
                         <div class="rectangle btn " style="padding-left: 0px; ">
                             <button type="button " class="btn btn-outline-primary ">Primary</button>
                         </div>
-                    </div>
+                    </div>	
                 </div>
             </div>
             <div class="image ">
-                <img src="test.png " alt="test " style="width:525px; height:400px; border-radius:2%;">
+                <img src="../images/art.png"" " style="width:525px; height:400px; border-radius:2%;">
                 <!-- <img src="test.png " alt="test " style="width:100%; height:100%; border-radius:2%;"> -->
             </div>
         </div>

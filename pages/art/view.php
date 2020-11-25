c8cd249e51793e01a3dcf7e8f9242640d43501aa
azsos741 <53871216+azsos741@users.noreply.github.com>
2020-11-25 18:17:51 +0700

    Art
    25/11/2020

--- pages/art/view.php
+++ pages/art/view.php
@@ -11,9 +11,24 @@
         <div class="row">
             <div class="col-lg-8">
                 <div class="row">
-                    <div class="col-md-4">Test1</div>
-                    <div class="col-md-4">Test2</div>
-                    <div class="col-md-4">Test3</div>
+                    <div class="col-md-4 " style="border: 1px solid #F1EAEA; padding:2%;">
+                        <div class="col-sm-4"></div>
+                        <div class="col-sm-8">
+                            <p>Credit Card</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 " style="border: 1px solid #F1EAEA; padding:2%;">
+                    <div class="col-sm-4"></div>
+                        <div class="col-sm-8">
+                            <p>Mobile Payment</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 " style="border: 1px solid #F1EAEA; padding:2%">
+                    <div class="col-sm-4"></div>
+                        <div class="col-sm-8">
+                            <p>Other method</p>
+                        </div>
+                    </div>
                 </div>
                 <div class="row">
                 <input type="text" class="form-control" placeholder="Name" aria-label="Name" aria-describedby="basic-addon2" style="margin-top: 1%;">
@@ -24,10 +39,29 @@
                 <input type="text" class="form-control" placeholder="Address" aria-label="Address" aria-describedby="basic-addon2" style="margin-top: 1%;">
                 <input type="text" class="form-control" placeholder="Zip or postal code" aria-label="postal" aria-describedby="basic-addon2" style="margin-top: 1%;">
                 <input type="text" class="form-control" placeholder="Card number" aria-label="card" aria-describedby="basic-addon2" style="margin-top: 1%;">
+
+                    <div class="row" style="margin-top: 1%; margin-left: 0%;">
+                        <?Php
+                        echo "<select name=month>";
+                        for($i=0;$i<=11;$i++){
+                        $month=date('F',strtotime("first day of -$i month"));
+                        echo "<option value=$month>$month</option> ";
+                        }
+                        echo "</select>";
+
+                        echo "<select name=year>";
+                        for($i=0;$i<=100;$i++){
+                        $year=date('Y',strtotime("last day of -$i year"));
+                        echo "<option name='$year'>$year</option>";
+                        }
+                        echo "</select>";
+                        ?>
+                    </div>
+
                 </div>
-                <div class="row" style="padding-top: 2%;">Test</div>
+                <div class="row" style="padding-top: 2%;"></div>
             </div>
-            <div class="col-lg-4" >
+            <div class="col-lg-4" style="border: 1px solid #F1EAEA; padding:2%;" >
                 <div class="container" style="width: 100%;">
                     <div class="row">
                         <h2>ORDER SUMMARY</h2>
@@ -73,7 +107,9 @@
                 </div>
             </div>
         </div>
-        <div class="row"></div>
+        <div class="row">
+        <a href=""  class="Button" style="padding: 4px;  margin: 20px; width: 15%; text-align: center; background: linear-gradient(90deg, #0056f7 0%, #ff3ee7 100%);    border: 1px solid #ffffff; margin-left:0px;" >PURCHASE NOW</a>
+        </div>
     </div>
 
 </body>

6d50f22c2548d8f9a6da76bf4b033019a00cee8f
azsos741 <53871216+azsos741@users.noreply.github.com>
2020-12-22 17:37:36 +0700

    Art
    Art

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -6,36 +6,50 @@ include('../php/function.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<?php
-	
+	<style >
+		
 
-	if(!isset($_GET['action']) ){
-		$cus = calendars($conn);
-		require_once('admin_pages/teacher/index.php');
-	}
-	if($_GET['action'] == 'asd'){
-		$s = calendars($conn);
-		//$cus = updateidcalendar($conn,$_POST);
-		//$cus = selectmax($conn);
-		//print_r($cus);
-		require_once('admin_pages/teacher/index.php');
-	}
+		<?php
+		$id = $_SESSION['id '];
+		$name = $_SESSION['name'];
+		$level = $_SESSION['level'];
+		$nlevel = $_SESSION['nlevel'];
 
-	if($_GET['action'] == 'admin_course'){
-		require_once('admin_pages/camp/index.php');
-	}
-	if($_GET['action'] == 'camp'){
-		require_once('admin_pages/camp/index.php');
-	}
-	if($_GET['action'] == 'insert_datetime'){
-		$cus = insertData($conn,$_POST);
-		$suc = calendars($conn);
-		require_once("admin_pages/teacher/teacher_list.php");
-	}
-	?>
+		if ($level=='admin') {
+
+			if(!isset($_GET['action']) ){
+				$cus = calendars($conn);
+				require_once('admin_pages/teacher/index.php');
+			}
+			if($_GET['action'] == 'asd'){
+				$s = calendars($conn);
+				//$cus = updateidcalendar($conn,$_POST);
+				//$cus = selectmax($conn);
+				//print_r($cus);
+				require_once('admin_pages/teacher/index.php');
+			}
+
+			if($_GET['action'] == 'admin_course'){
+				require_once('admin_pages/course/index.php');
+			}
+			if($_GET['action'] == 'camp'){
+				require_once('admin_pages/camp/index.php');
+			}
+			if($_GET['action'] == 'insert_datetime'){
+				$cus = insertData($conn,$_POST);
+				$suc = calendars($conn);
+				require_once("admin_pages/teacher/teacher_list.php");
+			}
+		}
+		?>
+	</style>
 </head>
 <body>
-	<?php require_once('admin_view/navber_vertical.php'); ?>
-	<?php require_once('admin_view/admin_footer.php'); ?>
+	<?php
+	if ($level=='admin') {
+		require_once('admin_view/navber_vertical.php'); 
+		require_once('admin_view/admin_footer.php'); 
+	}
+	?>
 </body>
 </html> 

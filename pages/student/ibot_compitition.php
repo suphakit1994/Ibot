414b1babbf72cad881dfadde57af05a42a47850b
azsos741 <53871216+azsos741@users.noreply.github.com>
2020-12-24 11:15:47 +0700

    Art
    Art

--- pages/student/ibot_compitition.php
+++ pages/student/ibot_compitition.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+<style type="text/css" media="screen">
+
+</style>
 <body>
-    <div class="container">
+    <div class="container" style="padding-bottom: 5%;">
         <div class="row" style="text-align: center;">
-            <h1 style="font-weight: bold;">IBOT CAMP</h1>
+            <h1 style="font-weight: bold;">IBOT COMPITITION</h1>
             <p style="padding-left: 30%; padding-right:30%;">Loerm ipsum dolor sit amet. consectetuer adipiscing edit. sed diam
             nonummy nibh euismod tinciduct urt laoreet dolore mayna</p>
         </div>
@@ -22,22 +25,24 @@
                 </select>
             </div>
         </div>
-        <div class="row" style="background-color: #F1EAEA; position: absolute; margin-top: 2%; margin-right:45%; border-radius: 2%; padding-left:2%;">
+        <div class="row" style="background-color: #F1EAEA; position: absolute; margin-top: 5%; padding-top: 2%; padding-bottom: 3%; margin-right:45%; border-radius: 2%; padding-left:2%;">
             <h1 style="font-weight: bold;">FIRST® LEGO®</h1>
             <h1 style="font-weight: bold;">League Season Challenges</h1>
             <div class="col-sm-10">            
-                <div class="row"style="padding-top:2%;" >
+                <div class="row"style="padding-top:2%; padding-bottom: 5%;" >
                     <p style="font-weight:lighter;">Every year,FIRST League releases a new Challenge based on a real world scientific theme.</p>
                     <p style="font-weight:lighter;">Starting with Discover, children are introduced to the fundamentals of STEM while working together to solve fun challenge and buildubg models using LEGO DUPLO bricks.</p>
                     <p style="font-weight:lighter;">As participants progress into Explore, children will take their background knowledge of STEM and put it into parctice as they work in teams to design and build robots using the LEGO Education WeDo 2.0 system</p>
                     <p style="font-weight:lighter;">Once they are ready to move into a competitive setting, they join Challenge and apply their STEM skills combined with critical thinking to work with a team, build a robot, and compete in an exciting, mission based robotics game.</p>
-                    <a href=""  class="Button" style="padding: 4px;  margin: 30px; width: 30%; text-align: center; background: linear-gradient(90deg, #0056f7 0%, #ff3ee7 100%);    border: 1px solid #ffffff; margin-left:0px;" >Enroll</a>
                 </div>
             </div>
         </div>
         <div class="row" style="position: relative;">
-            <img src="../images/images.png" style="width:45%; height:40%; border-radius:2%; margin-left:50%; margin-top:5%;">
+            <img src="../images/images.png" style="width:45%; height:40%; border-radius:2%; margin-left:50%; margin-top:10%;">
 
+        </div>
+        <div class="row" style="position: relative;">
+            <a  href="index.php?app=student&action=all_compitition" type="button" name="next" class="Button" style="border-radius:28px; width: 150px; margin-top: 50px;">Enroll</a>
         </div>
     </div>
 </body>

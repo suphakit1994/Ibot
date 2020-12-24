4afbfca28f6f5c16e32576fff47ef537f3d09244
azsos741 <53871216+azsos741@users.noreply.github.com>
2020-12-24 11:15:17 +0700

    Art
    Art

--- pages/student/ibot_camp.php
+++ pages/student/ibot_camp.php
@@ -1,35 +1,38 @@
-<style>
-    .btn {
-        color: #ffffff;
-        background-color: #dedede ;
-    }         
-    .btn:hover {
-        border-radius:28px;
-        border:1px solid #18ab29;
-        display:inline-block;
-        cursor:pointer;
-        color:#ffffff !important;
-        font-size:14;
-        text-decoration:none;
-        text-align: center; 
-        background: linear-gradient(90deg, #0050ef 0%, #ff5894 100%);    
-        border: 1px solid #ffffff;
-    }    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<style type="text/css" media="screen">
+
 </style>
 <body>
-    <div class ="container-fluid"  style="padding-right:80px ;padding-right:100px ;padding-left:100px ; padding-top: 5%;" >
+    <div class="container" style="padding-bottom: 5%;">
         <div class="row" style="text-align: center;">
-            <div class="col-md-3">
+            <h1 style="font-weight: bold;">IBOT CAMP</h1>
+            <p style="padding-left: 30%; padding-right:30%;">Loerm ipsum dolor sit amet. consectetuer adipiscing edit. sed diam
+            nonummy nibh euismod tinciduct urt laoreet dolore mayna</p>
+        </div>
+        <div class="row" style="background-color: #F1EAEA; position: absolute; margin-top: 5%; padding-top: 2%; padding-bottom: 3%; margin-right:45%; border-radius: 2%; padding-left:2%;">
+            <h1 style="font-weight: bold;">FIRST® LEGO®</h1>
+            <h1 style="font-weight: bold;">League Season Challenges</h1>
+            <div class="col-sm-10">            
+                <div class="row"style="padding-top:2%; padding-bottom: 5%;" >
+                    <p style="font-weight:lighter;">Every year,FIRST League releases a new Challenge based on a real world scientific theme.</p>
+                    <p style="font-weight:lighter;">Starting with Discover, children are introduced to the fundamentals of STEM while working together to solve fun challenge and buildubg models using LEGO DUPLO bricks.</p>
+                    <p style="font-weight:lighter;">As participants progress into Explore, children will take their background knowledge of STEM and put it into parctice as they work in teams to design and build robots using the LEGO Education WeDo 2.0 system</p>
+                    <p style="font-weight:lighter;">Once they are ready to move into a competitive setting, they join Challenge and apply their STEM skills combined with critical thinking to work with a team, build a robot, and compete in an exciting, mission based robotics game.</p>
+                </div>
             </div>
-            <div class="col-md-6">
-                <h1 style="font-weight: bold;"> IBOT CAMP </h1> 
-                <P>Loerm ipsum dolor sit amet. consectetuer adipiscing edit. sed diam nonummy nibh euismod tinciduct urt laoreet dolore mayna.</P>
-                <button class=" btn btn:hover" style="width:30% ;border-radius:28px;" type="submit" class="Button" style= "width: 30%" >Enroll</button>  
-                <button class=" btn btn:hover" style="width:30% ;border-radius:28px;" type="submit" class="Button" style= "width: 30%" >Details</button>    
-            </div>
-            <div class="col-md-3">
-            </div> 
-            <img class="card-img-top" src="../images/img_5terre.jpg" alt="Card image cap" style="padding-top: 100px;">   
+        </div>
+        <div class="row" style="position: relative;">
+            <img src="../images/camp.jpg" style="width:45%; height:40%; border-radius:2%; margin-left:50%; margin-top:19%;">
+
+        </div>
+        <div class="row" style="position: relative;">
+            <a  href="index.php?app=student&action=all_camp" type="button" name="next" class="Button" style="border-radius:28px; width: 150px; margin-top: 50px;">Enroll</a>
         </div>
     </div>
 </body>
+</html>

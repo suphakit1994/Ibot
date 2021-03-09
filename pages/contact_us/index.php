19cb5f0a9e8ba2b24e05e4038994a0c59e974def
azsos741 <53871216+azsos741@users.noreply.github.com>
2021-03-09 16:51:08 +0700

    Art
    Art

--- pages/contact_us/index.php
+++ pages/contact_us/index.php
@@ -1,16 +1,12 @@
 <?php
 date_default_timezone_set("Asia/Bangkok");
 include("../php/config.php");
-include('../php/test.php');	
-include('../php/art_ft.php');
+
 include('../php/function.php');
 
 
 if(!isset($_GET['action'])){
 	require_once('contact_us.php');
 }
-if($_GET['action'] == 'submit_text'){
-	$cus = contact($conn,$_POST);
-	require_once("contact_us.php");
-}
+
 ?>
